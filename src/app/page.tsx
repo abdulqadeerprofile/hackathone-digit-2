d4477b57ba77ab4ad5e160e048d6f61fd173b2a8
unknown <abdulqadeerprofile@gmail.com>
2024-12-09 01:16:18 +0500

    new changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,36 +2,16 @@ import dynamic from "next/dynamic";
 import Features from "@/components/features/Features/Features";
 import JoinUs from "@/components/features/JoinUs/JoinUs";
 import Page from "@/components/shared/Page/Page";
-import ListingsSkeleton from "@/components/shared/ListingsSkeleton/ListingsSkeleton";
+
 import Hero from "./hero";
 import Info from "./info";
-
-const NewProductListings = dynamic(
-  () => import("@/components/features/NewProductListings/NewProductListings"),
-  {
-    ssr: false,
-    loading: () => <ListingsSkeleton />,
-  }
-);
-
-const PopularProductListings = dynamic(
-  () =>
-    import(
-      "@/components/features/PopularProductListings/PopularProductListings"
-    ),
-  {
-    ssr: false,
-    loading: () => <ListingsSkeleton />,
-  }
-);
+  
 
 export default function Home() {
   return (
     <Page>
       <Hero />
-      <Features />
-      <NewProductListings />
-      <PopularProductListings />
+      <Features />      
       <JoinUs />
       <Info />
     </Page>
